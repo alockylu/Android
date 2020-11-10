2966ec94e620513574b9056a7261e3eaa4982887
alockylu <alockylu@gmail.com>
2020-11-10 22:34:03 +0200

    Лабораторна робота №4

--- app/src/main/java/com/chnulabs/employees/EmployeesListActivity.java
+++ app/src/main/java/com/chnulabs/employees/EmployeesListActivity.java
@@ -1,15 +1,19 @@
 package com.chnulabs.employees;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.TypedValue;
 import android.view.View;
 import android.widget.TextView;
 
 public class EmployeesListActivity extends AppCompatActivity {
 
     public static final String DEPARTMENT_NAME = "employees_list";
+
+    private float textSize;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,8 +28,20 @@ public class EmployeesListActivity extends AppCompatActivity {
 
         TextView textView = findViewById(R.id.employees_txt);
         textView.setText(txtEmployees);
+
+        textSize = textView.getTextSize();
+
+        if (savedInstanceState != null) {
+            textSize = savedInstanceState.getFloat("textSize");
+            textView.setTextSize(TypedValue.COMPLEX_UNIT_PX, textSize);
+        }
     }
 
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putFloat("textSize", textSize);
+    }
 
     public void onSendBtnClick(View view) {
         TextView textView = findViewById(R.id.employees_txt);
@@ -34,6 +50,12 @@ public class EmployeesListActivity extends AppCompatActivity {
         sendIntent.setType("text/plain");
         sendIntent.putExtra(Intent.EXTRA_TEXT, textView.getText());
         sendIntent.putExtra(Intent.EXTRA_SUBJECT, "Employees List");
+
         startActivity(sendIntent);
+    }
+
+    public void onFontBtnClick(View view) {
+        textSize *= 1.1;
+        ((TextView)findViewById(R.id.employees_txt)).setTextSize(TypedValue.COMPLEX_UNIT_PX, textSize);
     }
 }
--- app/src/main/java/com/chnulabs/employees/MainActivity.java
+++ app/src/main/java/com/chnulabs/employees/MainActivity.java
@@ -1,26 +1,73 @@
 package com.chnulabs.employees;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int seconds = 0;
+    private boolean isRunning = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        runTimer();
     }
 
-    public void onBtnClick(View view) {
+    public void onShowBtnClick(View view) {
         Spinner spinner = findViewById(R.id.dep_spinner);
         String department = (String) spinner.getSelectedItem();
 
         Intent listIntent = new Intent(this, EmployeesListActivity.class);
         listIntent.putExtra(EmployeesListActivity.DEPARTMENT_NAME, department);
         startActivity(listIntent);
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("seconds", seconds);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        isRunning = true;
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        isRunning = false;
+    }
+
+    private void runTimer() {
+        final TextView textView = findViewById(R.id.timer);
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int hours = seconds / 3600;
+                int minutes = seconds % 3600 / 60;
+                int secs = seconds % 60;
+
+                String time = String.format(Locale.getDefault(), "%d:%02d:%02d", hours, minutes, secs);
+                textView.setText(time);
+                if (isRunning) {
+                    seconds++;
+                }
+                handler.postDelayed(this, 1000);
+            }
+        });
     }
 }
